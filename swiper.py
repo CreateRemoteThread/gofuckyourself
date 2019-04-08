1f9ecac315b02ef3800178656001dc6e3fb2d823
a <lol@mailinator.com>
2019-04-08 18:06:13 +1000

    save

--- swiper.py
+++ swiper.py
@@ -12,39 +12,57 @@ import numpy as np
 import csv
 
 class GlitchDataManager:
-  def __init__(self,prefix):
-    self.csvname = "%s-output.csv" % prefix
-    self.npzname = "%s-traces.npz" % prefix
+  def __init__(self):
+    self.rdb = {}
+    self.dataIndex = 0
+    pass
 
-  def scatterWinCases(self):
-    sr = csv.reader(open(self.csvname),delimiter=',',quotechar='\'')
+  def loadData(self, prefix):
+    f = open("%s-output.csv" % prefix)
+    sr = csv.reader(f,delimiter=',',quotechar='\'')
+    for r in sr:
+      (output,hash,ext_offset,offset,width,repeat,success) = r
+      if output == "Output":
+        continue
+      self.rdb[self.dataIndex] = (output,hash,int(ext_offset),float(offset),float(width),int(repeat),success == "True")
+      self.dataIndex += 1
+    f.close()
+
+  def scatterWinCases(self,ignore="repeats"):
     p_ext_offset = []
     p_offset = []
     p_width = []
     p_repeat = []
-    for row in sr:
+    p_hashes = []
+    for i in range(0,self.dataIndex):
+      row = self.rdb[i]
       (output,hash,ext_offset,offset,width,repeat,success) = row
-      if success == "True":
+      if success == True:
         p_ext_offset.append(int(ext_offset))
         p_offset.append(float(offset))
         p_width.append(float(width))
         p_repeat.append(int(repeat))
+        p_hashes.append(hash)
+    p_hashmapc = []
+    for i in range(0,len(p_hashes)):
+      p_hashmapc.append(p_hashes.count(p_hashes[i]))
     fig = plt.figure()
     ax = fig.add_subplot(111,projection='3d')
-    ax.scatter(p_ext_offset,p_offset,p_width)
+    ax.scatter(p_ext_offset,p_offset,p_width,c=p_hashmapc,cmap='viridis')
     ax.set_xlabel("Ext. Offset")
     ax.set_ylabel("Offset")
     ax.set_zlabel("Width")
+    plt.title("Glitch Successes")
+    ax.legend()
     plt.show()    
 
 mpl.rcParams['agg.path.chunksize'] = 10000
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
+  if len(sys.argv) == 1:
     print "usage: ./swiper.py [set-prefix]"
     sys.exit(0)
-  if not (os.path.isfile("%s-output.csv" % sys.argv[1]) and os.path.isfile("%s-traces.npz" % sys.argv[1])):
-    print "Cannot find %s-output.csv or %s-traces.npz" % (sys.argv[1],sys.argv[1])
-    sys.exit(0)
-  gdm = GlitchDataManager(sys.argv[1])
+  gdm = GlitchDataManager()
+  for i in range(0,len(sys.argv[1:])):
+    gdm.loadData(sys.argv[1 + i])
   gdm.scatterWinCases()
